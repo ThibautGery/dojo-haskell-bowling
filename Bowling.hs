1a34041c792ca16cbc2d8c170aa4eeb7f0e1cbe0
Thibaut Gery <thibaut.gery@gmail.com>
2017-06-07 00:03:12 +1000

    add tird  case, deal with spare

--- Bowling.hs
+++ Bowling.hs
@@ -2,9 +2,7 @@ module Bowling
 where
 
 score :: [Integer] -> Integer
-score [] = 0
-score (x : []) = x
-score (x : y : []) = x + y
 score (x : y : z : xs)
   | x + y == 10 = x + y + z + score (z : xs)
   | otherwise = x + y + score (z : xs)
+score list = sum list
